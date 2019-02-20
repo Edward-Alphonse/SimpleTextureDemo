f0884ea3544780286dc5b5ab4c19dc9412dcfb10
zhichang.he <523011150@qq.com>
2019-02-21 00:55:35 +0800

    Initial Commit

---
+++ SimpleTextureDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleTextureDemo
+//
+//  Created by zhichang.he on 2019/2/21.
+//  Copyright © 2019年 zhichang.he. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SimpleTextureDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SimpleTextureDemo
+//
+//  Created by zhichang.he on 2019/2/21.
+//  Copyright © 2019年 zhichang.he. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
